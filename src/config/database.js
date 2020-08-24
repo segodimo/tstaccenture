3cb6409d260657881a46a1af7ad6e583073b6f86
segodimo <jsbachb612>
2020-08-24 09:40:38 -0300

    0940 db

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 module.exports = (app) => {
   const URI = process.env.MONGODB_URI
     ? process.env.MONGODB_URI
-    : 'mongodb://localhost/accndb';
+    : 'mongodb+srv://segodimo:4xTRFiByoRLr0TkD@cluster0.pehon.mongodb.net/tstaccenture?retryWrites=true&w=majority';
+    // : 'mongodb://localhost/accndb';
 
   mongoose.connect(URI, {
     useNewUrlParser: true,
