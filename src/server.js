ed82cb140c3f3da7f28fc423de65b3e3ec0268ad
segodimo <jsbachb612>
2020-08-24 19:32:27 -0300

    1932

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(3001);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
