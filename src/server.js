f53b22c0c9264d9c322bea9fdd120225573f4067
segodimo <jsbachb612>
2020-08-24 20:16:01 -0300

    2015

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, function(){
+console.log('OKOKOKOKOKOKOKOKOKOKOKO');
+console.log(process.env.PORT,'process.env.PORT');
+
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
