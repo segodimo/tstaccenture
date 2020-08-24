a5e7449bd3cfcd4156d7820ea8a9246a3f3e9aa4
segodimo <jsbachb612>
2020-08-24 20:33:12 -0300

    2033

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = (app) => {
   const URI = process.env.MONGODB_URI
     ? process.env.MONGODB_URI
-    : 'mongodb+srv://segodimo:4xTRFiByoRLr0TkD@cluster0.pehon.mongodb.net/dbaccenture?retryWrites=true&w=majority';
+    : 'mongodb://localhost/dbaccenture';
 
   mongoose.connect(URI, {
     useNewUrlParser: true,
--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-console.log('AWAWAWAWAWAWAWAWAW');
-console.log(process.env.PORT,'process.env.PORT');
-
 app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
